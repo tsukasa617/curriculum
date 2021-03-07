0aeb1bcceb43c5f5e558f5f4e7e84cb8e1d531a7
高田司 <ttsukasa@gmail.com.jp>
2021-03-08 01:39:37 +0900

    課題提出テスト
    課題評価お願いいたします。

--- JavaStudy/1-5.javaファイル.java
+++ JavaStudy/1-5.javaファイル.java
@@ -1,33 +1,49 @@
-public class Check{
-  public static void main(String[] args){
-      /*
-      * 問1
-      * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
-      */int[] data={3,1,2,7,5,};
+Task1-5 : 課題内容
+ *
+ * 本課題では、ルーブ文に記述に慣れていきましょう。
+ * 問①〜問④まであります。
+ * for文・while文の仕組みを意識しながらコーディングしていきましょう！
+ */
+public class Task1_5 {
 
-      /*
-      * 問2
-      * 以下のfor文を完成させなさい
-      */
-      for(int i = 0; i < data.length; i++){
-          System.out.print(data[i] + " ");
-      }
-      System.out.println();
-      for (int i = 0; i <data.length ; i++) {
-          for (int j = data.length; j > i; j--) {
-              /*
-              * 問3
-              * 以下、配列の添字を入れてソートを完成させなさい
-              */
-              if(data[i] > data[j]){
-                int box = data[j];
-                data[i] = data[j];
-                data[i] = box;
-              }
+    public static void main(String[] args) {
+
+        // ① 「みかん」、「りんご」、「ぶどう」、「メロン」の値を設定した配列 fluits を作成してください。
+String[] fluits={"みかん","りんご","ぶどう","メロン",};
+
+        // ② for文を使って①で作成した配列を出力しなさい。
+        for (int i=0; i<fluits.length; i++){
+            System.out.println(fluits[i]);
+            }
+
+        // ③ 以下のwhile文の処理について、何をしているのかコメントを記入してください。
+        /*1から１００までの数字を出力している。
+        *  [ここに記入]
+        */
+        int i = 1;
+        while(i <= 100) {
+          System.out.print(i);
+          i++;
+        }
+        System.out.println();
+
+        /* ④ Wikiに掲載されている【多重ループのプログラム例 （九九表を表示）】を参考に、
+        *    行の最初に「段数」と「||」を追加したものを表示させるプログラムを作成しなさい。
+        *     1 || 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+        *     2 || 2 | 4 | 6 | 8 | 10 | 12 | 14 | 16 | 18 |
+        *     3 || 3 | 6 | 9 | 12 | 15 | 18 | 21 | 24 | 27 |
+        *     4 || 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 |
+        *     5 || 5 | 10 | 15 | 20 | 25 | 30 | 35 | 40 | 45 |
+        *     6 || 6 | 12 | 18 | 24 | 30 | 36 | 42 | 48 | 54 |
+        *     7 || 7 | 14 | 21 | 28 | 35 | 42 | 49 | 56 | 63 |
+        *     8 || 8 | 16 | 24 | 32 | 40 | 48 | 56 | 64 | 72 |
+        *     9 || 9 | 18 | 27 | 36 | 45 | 54 | 63 | 72 | 81 |
+        */for (int a=1; a<=9; a++){
+          System.out.print(a+"||");
+          for (int b=1; b<=9; b++){
+              System.out.print(a*b+"|");
           }
-      }
-      for(int i = 0; i < data.length; i++){
-          System.out.print(data[i] + " ");
-      }
-  }
+        System.out.println();
+         } 
+    }
 }
--- JavaStudy/check1.java
+++ JavaStudy/check1.java
@@ -13,17 +13,17 @@ public class Check{
           System.out.print(data[i] + " ");
           }
       
-      System.out.println();
-      for (int i = 0; i <data.length ; i++) {
-          for (int j = data.length; j > i; j--) {
-              /*
-              * 問3
-              * 以下、配列の添字を入れてソートを完成させなさい
-              */
-              if(data[i] > data[j]){
-                int box = data[j];
-                data[i] = data[j];
-                data[i] = box;
+          System.out.println();
+          for (int i = 0; i <data.length-1 ; i++) {
+              for (int j = data.length-1; j > i; j--) {
+                  /*
+                  * 問3
+                  * 以下、配列の添字を入れてソートを完成させなさい
+                  */
+                  if(data[j-1] > data[j]){
+                    int box = data[j];
+                    data[j] = data[j-1];
+                    data[j-1] = box;
               }
           }
       }
